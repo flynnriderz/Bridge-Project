47815031b81c40c5340672f9ef9650e58e2f28af
Muhammed Rejwan Uddin <apu.toto.cat1@gmail.com>
2019-11-14 07:06:36 +0700

    Test Case

--- bridge_wizard/lib/main.dart
+++ bridge_wizard/lib/main.dart
@@ -1,11 +1,63 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+import './question.dart';
+import './answer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget{
+  
+  @override
+  //_MyAppState createState() => _MyAppState();
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+    var questions = [
+      {
+        'questionText': 'What\'s your favorite color?', 
+        'answer': ['Black', 'Red', 'Green', 'White'],
+      },
+      {
+        'questionText': 'What\'s your favorite animal?', 
+        'answer': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      },
+      {
+        'questionText': 'Who\'s your favorite professional footballer?', 
+        'answer': ['C. Ronaldo', 'L. Messi', 'K. Mbappe', 'E. Hazard'],
+      },
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Bridge Wizard'),
+        ),
+        body: Column(
+          children: [
+            Question(
+              questions[_questionIndex]['questionText'],
+            ),
+            ...(questions[_questionIndex]['answer'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList() 
+          ],
+        ),
+      ),
+    );
   }
 }
